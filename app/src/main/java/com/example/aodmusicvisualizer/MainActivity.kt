995191ffecbd2a2b6be694e0adb721c88f3995a9
tayadehritik <tayadehritik@gmail.com>
2023-08-17 22:59:40 +0530

    #1 Top App Bar -> done

--- app/src/main/java/com/example/aodmusicvisualizer/MainActivity.kt
+++ app/src/main/java/com/example/aodmusicvisualizer/MainActivity.kt
@@ -1,33 +1,108 @@
 package com.example.aodmusicvisualizer
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.aodmusicvisualizer.ui.theme.AODMusicVisualizerTheme
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Switch
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Switch as Material3Switch
+
+
+
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            AODMusicVisualizerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
+            AODMusicVisualizerTheme() {
+                TopAppBar()
             }
+
+
+
         }
     }
 }
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
+
+
+@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar() {
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+
+                    ) {
+
+                        Text(
+                            "AOD Music Visualizer",
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        Divider(modifier = Modifier.padding(0.dp,10.dp))
+
+                    }
+
+                }
+            )
+        },
+        content = {  innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = innerPadding.calculateTopPadding(), start = 10.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "Permissions Granted"
+                )
+            }
+
+        }
+    )
+}
+
+
+
 
 @Composable
 fun Greeting(name: String) {
@@ -38,6 +113,8 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     AODMusicVisualizerTheme {
-        Greeting("Android")
+        TopAppBar()
     }
 }
+
+
