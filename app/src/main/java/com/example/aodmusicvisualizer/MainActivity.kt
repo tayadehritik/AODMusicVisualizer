f99dc5cc3181390e610f4301035b577f6785b69d
Hritik Tayade <tayadehritik@gmail.com>
2023-12-10 16:52:27 +0530

    working on bar visualizer preview

--- app/src/main/java/com/example/aodmusicvisualizer/MainActivity.kt
+++ app/src/main/java/com/example/aodmusicvisualizer/MainActivity.kt
@@ -4,17 +4,18 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.aodmusicvisualizer.ui.theme.AODMusicVisualizerTheme
 import android.Manifest
+import android.animation.ValueAnimator
+import android.graphics.RuntimeShader
+import android.view.animation.LinearInterpolator
+import androidx.compose.foundation.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
@@ -33,10 +34,65 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Card
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Shader
+import androidx.compose.ui.graphics.ShaderBrush
+import androidx.compose.ui.text.style.TextAlign
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
+
+private const val DURATION = 4000f
+private const val COLOR_SHADER_SRC = """
+   uniform float2 iResolution;
+   uniform float iTime;
+   
+   float drawRect(vec2 coords)
+   {
+        vec2 bl = step(vec2(0.1),coords);
+        float pct = bl.x * bl.y;
+        
+        vec2 tr = step(vec2(0.1),1.0-coords);
+        pct = pct * tr.x * tr.y;
+        
+        vec2 bl2 = step(vec2(0.2), coords);
+        vec2 tr2 = step(vec2(0.2),1.0-coords);
+        float pct2 = bl2.x * bl2.y * tr2.x * tr2.y;
+        pct = pct - pct2;
+        return pct;
+   }
+   
+   float drawBars(vec2 coords)
+   {
+        
+        
+        float pct = step(0.2,mod((coords.x*10),0.8));
+        float valy = 1.0 - (step(abs(sin(coords.x*2.0+iTime))-0.1 ,coords.y));
+        pct *= valy;
+        
+        return pct;
+   }
+   
+   half4 main(float2 fragCoord) 
+    {
+       float2 st = fragCoord/iResolution.xy;
+       st.y = 1.0-st.y;
+       vec3 color = vec3(0.0);
+
+       color = vec3(drawBars(st));
+       
+       return half4(color,1.0);
+    }
+"""
+// created as top level constants
+val colorShader = RuntimeShader(COLOR_SHADER_SRC)
+val shaderBrush = ShaderBrush(colorShader)
+// declare the ValueAnimator
+private val shaderAnimator = ValueAnimator.ofFloat(0f, DURATION)
+
+// use it to animate the time uniform
+
+
 
 var modifyAudioSettingsPermissionGranted = false
 var recordAudioPermissionGranted = false
@@ -48,6 +104,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         setContent {
 
             AODMusicVisualizerTheme() {
@@ -57,17 +114,10 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
-)
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-    name = "DefaultPreviewLight"
-)
 
 
-@Preview(showBackground = true)
+
+
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun TopAppBar() {
@@ -128,32 +178,13 @@ fun TopAppBar() {
                 Column(modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.spacedBy(10.dp))
                 {
-                    for(i in 0..1)
+                    for(i in 1..1)
                     {
                         Row(modifier = Modifier
                             .fillMaxWidth(),
                             horizontalArrangement = Arrangement.SpaceEvenly)
                         {
-                            OutlinedCard(
-                                Modifier
-                                    .fillMaxWidth()
-                                    .weight(1f)
-                                    .height(100.dp))
-                            {
-                                // Card content
-                            }
-                            Spacer(modifier = Modifier.width(10.dp))
-                            OutlinedCard(
-                                Modifier
-                                    .fillMaxWidth()
-                                    .weight(1f)
-                                    .height(100.dp))
-                            {
-                                // Card content
-
-
-                            }
-
+                            visualizerCard()
                         }
                     }
 
@@ -165,13 +196,54 @@ fun TopAppBar() {
     )
 }
 
-@Preview(showBackground = true)
+
+
+
+@Preview
 @Composable
-fun DefaultPreview() {
-    AODMusicVisualizerTheme {
-        TopAppBar()
+fun visualizerCard(name:String="test" )
+{
+    val speed: Float = 0.01f
+    val time = remember { mutableStateOf(0f) }
+    LaunchedEffect(Unit) {
+        do {
+            withFrameMillis {
+                time.value = time.value + speed
+            }
+        } while (true)
     }
+    OutlinedCard(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        border = BorderStroke(1.dp, Color.Black),
+        modifier = Modifier.fillMaxWidth().height(400.dp)
+
+
+    ) {
+        Text(
+            text = "Bar Vizualizer",
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+        )
+        Canvas(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            colorShader.setFloatUniform("iResolution",
+                size.width, size.height)
+            colorShader.setFloatUniform(
+                "iTime",
+                time.value
+            )
+
+            drawRect(brush = shaderBrush)
+        }
+
+    }
+
 }
+
 
 
 @OptIn(ExperimentalPermissionsApi::class)
